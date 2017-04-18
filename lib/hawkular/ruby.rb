f252c0681961af1ba7ffb718b1518876ceccfb6f
Ben Long <14benj@gmail.com>
2017-04-18 10:17:24 -0400

    Adding dependencies

--- lib/hawkular/ruby.rb
+++ lib/hawkular/ruby.rb
@@ -1,6 +1,8 @@
 require 'hawkular/apm_tracer'
+require 'hawkular/apm_span'
 require 'hawkular/apm_span_context'
 require 'hawkular/basic_utils'
+require 'hawkular/constants'
 require 'hawkular/deployment_meta_data'
 require 'hawkular/recorder'
 require 'hawkular/reference'
